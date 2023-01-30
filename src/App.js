e5d89bf25e46eab32dbabe87a32c4435638f9033
JakeTurnick <jaketurnick@gmail.com>
2023-01-30 15:12:48 -0500

    button increments likes, reset button resets like count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const addLikes = () => setCount(count + 1);
+  const displayLikes = () => {
+    if (count == 1) {
+      return `${count} like`;
+    } else {
+      return `${count} likes`;
+    }
+  };
+  const resetLikes = () => setCount(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="addLike" onClick={addLikes}>
+        {displayLikes()}
+      </button>
+      <button className="reset" onClick={resetLikes}>
+        Reset likes!
+      </button>
     </div>
   );
 }
